e872cc7b0784bb98f2ccc1bf00e847e65eaaba9a
CRRodri <144954672+CRRodri@users.noreply.github.com>
2023-10-30 22:12:12 -0500

    Update VisitorPage.js

--- src/VisitorPage.js
+++ src/VisitorPage.js
@@ -1,6 +1,6 @@
 import './VisitorPage.css';
 import React, { useState } from 'react';
-
+import {Link} from "react-router-dom";
 function App() {
   const [visible, setVisibleSection] = useState('section1');
 
