c0c682341856e143a0c9d88f2ed17d8af581ecc3
CRRodri <144954672+CRRodri@users.noreply.github.com>
2023-11-01 13:58:18 -0500

    Update VisitorPage.js

--- src/VisitorPage.js
+++ src/VisitorPage.js
@@ -8,6 +8,13 @@ function App() {
     setVisibleSection(section);
   }
   
+  const [vis, setVisSection] = useState('');
+
+  const showSections = (section) => {
+    setVisSection(section);
+  }
+  
+  
   return (
     <div className="App">
       <ul className="nav-headers">
@@ -44,23 +51,155 @@ function App() {
 
             <div style={{ display: visible === 'section1' ? 'block' : 'none' }}>
               <div className="optiontextbox">
-                <h2>Your Account Information</h2>
+                <h2>Your Account Information - VisitorID:</h2>
                   <div class = "Account-Info">
-                    <span class = "FirstNamelabel" >Account Username: </span>
-                    <br>
-                    </br>
-                    <span class = "FirstNamelabel" >First Name: </span>
-                    <br>
-                    </br>
-                    <span class = "LastNamelabel" >Last Name: </span>
+                  <table>
+                      <tr>
+                        <th>First Name </th>
+                        <th>Last Name </th>
+                        <th>Username </th>
+                        <th>Email </th>
+                        <th>Adress </th>
+                        <th>Payment Method </th>
+                      </tr>
+                      <tr>
+                        <td>Jane</td>
+                        <td>Doe</td>
+                        <td>JaneUSer</td>
+                        <td>EmailJaneUser@jane.com</td>
+                        <td>EmailJaneUser Street</td>
+                        <td>124324324324</td>
+                      </tr>
+                  </table>
+                  <p> Update the above information
+                  </p>
+                  
+                    <div className="UpdateAccInfoSection">
+                      <form id="edit" method="POST" action="">
+                          <p>
+                              <label for="FirstName ">First Name:  </label>
+                              <input id="FName" type="text" name="username1" value=""/>
+                              <label for="LastName">Last Name: </label>
+                              <input id="LName" type="text" name="username1" value=""/>
+                              <label for="Username">Username: </label>
+                              <input id="UName" type="text" name="username1" value=""/>
+                              <label for="Email">Email: </label>
+                              <input id="Email" type="text" name="username1" value=""/>
+                              <label for="Address">Address: </label>
+                              <input id="Address" type="text" name="username1" value=""/>
+                              <label for="PaymentMethod">Payment Method: </label>
+                              <input id="PayMethod" type="text" name="username1" value=""/>
+                          </p>
+                          <p>
+                              <button id="update1submit" type="submit">Submit</button>
+                          </p>
+
+                      </form>
+                      <h2> Security Information
+                       </h2>
+                       <table>
+                      <tr>
+                        <th>Current Password </th>
+                      </tr>
+                      <tr>
+                        <td>Jane234324</td>
+                      </tr>
+                      </table>
+                  <p> Do you want to change you password?</p>
+                  <div className="UpdateAccPasSection">
+                      <form id="edit" method="POST" action="">
+                          <p>
+                              <label for="Password ">New Password:  </label>
+                              <input id="Password" type="text" name="username1" value=""/>
+  
+                          </p>
+                          <p>
+                              <button id="update1submit" type="submit">Submit</button>
+                          </p>
+
+                      </form>
+                   </div>
+                   </div>
                   </div>
               </div>
             </div>
 
             <div style={{ display: visible === 'section2' ? 'block' : 'none' }}>
               <div className="optiontextbox">
-                <h2>Buy a ticket!</h2>
-                <p>Section 2 content goes here.get you ticketsget you ticketget you ticketget you ticketget you ticketget you ticket please ..</p>
+                <h2>Purchase a ticket</h2>
+                <p>Ticket Information</p>
+                <table>
+                      <tr>
+                        <th>Ticket Type </th>
+                        <th>Benefits </th>
+                        <th>Cost </th>
+                      </tr>
+                      <tr>
+                        <td>Day Pass</td>
+                        <td>$33</td>
+                        <td>Ride Photos or Souvenirs</td>
+                      </tr>
+                      <tr>
+                        <td>Seasonal Pass</td>
+                        <td>$61</td>
+                        <td>Special Shows or Entertainment</td>
+                      </tr>
+                      <tr>
+                        <td>Annual Pass</td>
+                        <td>$151</td>
+                        <td>Discounts on Merchandise & Dining plan </td>
+                      </tr>
+                      <tr>
+                        <td>Premium Pass</td>
+                        <td>$351</td>
+                        <td>Fast Pass </td>
+                      </tr>
+                </table>
+                <p>Complete the section below to get your tickets</p>
+                <div className="PurchTicketSection">
+                      <form id="edit" method="POST" action="">
+                          <p>
+                              <label for="FirstName ">First Name:  </label>
+                              <input id="FName" type="text" name="username1" value=""/>
+                              <label for="LastName">Last Name: </label>
+                              <input id="LName" type="text" name="username1" value=""/>
+                              <label for="Address">Address: </label>
+                              <input id="Address" type="text" name="username1" value=""/>
+                              <label for="Email">Account Password: </label>
+                              <input id="Email" type="text" name="username1" value=""/>
+
+                              <label for="Username">Card Number: </label>
+                              <input id="UName" type="text" name="username1" value=""/>
+
+
+
+                          </p>
+                          <p>
+                              <button id="update1submit" type="submit">Submit</button>
+                          </p>
+
+                      </form>
+                  </div>
+                  <br></br><br></br><br></br><br></br>
+                  <h2>Previous Purchases</h2>
+                  <table class = "PrevTicketPurch" id = "PrevTicket">
+                  <tr>
+                    <th>Date:</th>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <th>Type:</th>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <th>Amount:</th>
+                    <td></td>
+                  </tr>
+                 </table>
+
+                 <p> You must show your customer ID before you can get into the park<br></br>This can be found in your account information.</p>
+
+
               </div>
             </div>
 
