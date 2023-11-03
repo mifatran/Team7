194374a415876bb72d8c02f5d5d8475b469be740
CRRodri <144954672+CRRodri@users.noreply.github.com>
2023-11-03 11:14:54 -0500

    Update MainRoutes.js

--- src/MainRoutes.js
+++ src/MainRoutes.js
@@ -4,6 +4,7 @@ import MainPg from "./MainPg";
 import Signin from "./Signin";
 import Signup from "./Signup";
 import Visitorpage from "./Visitorpage";
+import BusinessAnalyst from "./BusinessAnalyst";
 
 function MainRoutes() {
   return (
@@ -12,6 +13,7 @@ function MainRoutes() {
           <Route path="/Signin" Component={Signin} />
           <Route path="/Signup" Component={Signup} />
           <Route path="/Visitorpage" Component={Visitorpage} />
+          <Route path="/BusinessAnalyst" Component={BusinessAnalyst} />
         </Routes>
   );
 }
